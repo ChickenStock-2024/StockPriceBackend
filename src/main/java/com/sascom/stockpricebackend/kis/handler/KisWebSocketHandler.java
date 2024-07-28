2ed001506cd1e6b6cef6def12507d15b9ace4613
given-dragon <yijy001@gmail.com>
2024-07-28 18:15:03 +0900

    [Feat] #2 - 주식 데이터 파싱 적용

--- src/main/java/com/sascom/stockpricebackend/kis/handler/KisWebSocketHandler.java
+++ src/main/java/com/sascom/stockpricebackend/kis/handler/KisWebSocketHandler.java
@@ -1,13 +1,17 @@
 package com.sascom.stockpricebackend.kis.handler;
 
+import com.sascom.stockpricebackend.kis.model.ResolvedData;
 import com.sascom.stockpricebackend.kis.properties.StockName;
 import com.sascom.stockpricebackend.kis.properties.TrName;
 import com.sascom.stockpricebackend.kis.util.KisWebSocketUtil;
+import com.sascom.stockpricebackend.kis.util.OpsDataParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import static com.sascom.stockpricebackend.kis.util.OpsDataParser.PINGPONG_TR_ID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,19 +19,30 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class KisWebSocketHandler extends TextWebSocketHandler {
 
     private final KisWebSocketUtil kisWebSocketUtil;
+    private final OpsDataParser opsDataParser;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        log.info(message.getPayload());
+        String payload = message.getPayload();
+        log.info("[RECEIVE] : {}", payload);
+
+        ResolvedData<?> resolvedData = opsDataParser.resolveMessage(payload);
+
+        if (PINGPONG_TR_ID.equals(resolvedData.trId())) {
+            log.info("[SEND] : {}", payload);
+            session.sendMessage(new TextMessage(payload));
+            return;
+        }
+        if (resolvedData.data() != null) {
+            log.info("resolvedData: {}", resolvedData.data());
+        }
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("kis session connected: {}", session.getId());
-        kisWebSocketUtil.subscribe(session, TrName.HOKA, StockName.SAMSUNG);
-        kisWebSocketUtil.subscribe(session, TrName.PURCHASE, StockName.SAMSUNG);
-
-        session.close();
+        kisWebSocketUtil.subscribe(session, TrName.REALTIME_HOKA, StockName.SAMSUNG);
+        kisWebSocketUtil.subscribe(session, TrName.REALTIME_PURCHASE, StockName.SAMSUNG);
     }
 
     @Override
